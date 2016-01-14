6d1e6160b4a2c331fe7278c920b0dec9be5a49b8
Zach Cowell <zach.cowell@gmail.com>
2016-01-14 01:19:17 -0500

    Stub data being sent through XML and translated

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,26 @@
+var fs = require('fs')
+var parseString = require('xml2js').parseString;
+
 module.exports = function(app){
     app.get('/test', function(req, res){ res.status(200).send('this is the body') });
+
+    /**
+     * Entry point for XML parsing; this is stub data and should be replaced
+     */
+    app.get('/patient/data',function(req, res){
+        var tempFilepath = '/home/vagrant/src/app/data/cerner_ccda.xml';
+        fs.readFile(tempFilepath, 'utf8', function (err, xml) {
+          if (err) {
+            res.status(500).send('Error fetching XML');
+          }
+          parseString(xml, function (err, result) {
+            if (err) {
+                res.status(500).send('Error parsing XML');
+            }
+            res.status(200).send(result);  
+          });
+        });
+    });
+    // End stub
+
 }
